68f981d7c6a1988c0f638b8eeab74e7e4c13400a
GabrielGarciaRibeiroDeSouza <49999101+GabrielGarciaRibeiroDeSouza@users.noreply.github.com>
2022-05-20 22:33:38 -0300

    Add files via upload

--- Scripts/NavmeshTankMove.cs
+++ Scripts/NavmeshTankMove.cs
@@ -5,16 +5,22 @@ using UnityEngine.AI;
 
 public class NavmeshTankMove : MonoBehaviour
 {
+    //variavel publica para adicionar o agente
     public NavMeshAgent agent;
  
     void Update()
     {
+        //se clicar o botão esquerdo mouse
         if (Input.GetMouseButtonDown(0))
         {
+            //variavel para armazenar a posição do mouse
             RaycastHit hit;
+
+            //cria um raycast na posição do mouse
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 500))
-            {                
-                    agent.SetDestination(hit.point);
+            {       
+                //define o destino do agente igual a posição do "hit.point"(um vetor3) 
+                agent.SetDestination(hit.point);
             }
         }
     }
